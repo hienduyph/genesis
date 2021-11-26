2e3707b9a8e593f0e94272185f20c1cfe191c25b
Hien Pham <hienduyph@gmail.com>
2021-11-26 21:00:34 +0700

    sol(add): smartlink

---
+++ sols/contracts/FundMe.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.7/vendor/SafeMathChainlink.sol";
+
+contract FundMe {
+    using SafeMathChainlink for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+}
